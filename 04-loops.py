c486391fe1db023160f7f3f8a3ff02f922f21d7b
metal <metalheadkrn@gmail.com>
2026-05-17 18:04:08 +0530

    python basics

--- 04-loops.py
+++ 04-loops.py
@@ -4,4 +4,11 @@ fruits = ["apple", "banana", "orange"]
 
 for fruit in fruits:
     print(f"Fruit name is - {fruit}")
-    
+
+# while loop
+
+i = 5
+while i > 0:
+    print(f("Iteration - {i}"))
+    i = i - 1
+
