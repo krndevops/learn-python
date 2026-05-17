378a48efecc9685979f708359960c4575e5c0de0
metal <metalheadkrn@gmail.com>
2026-05-17 17:26:28 +0530

    python basics

--- conditions.py
+++ conditions.py
@@ -5,4 +5,11 @@ if age < 18:
     print("You are Minor")
 else
     print("You are Major")
-    
+
+
+if age < 18:
+    print("You are Minor")
+elif age >= 18 and age < 60:
+    print("You are an Adult")
+else
+    print("You are a senior citizen")
